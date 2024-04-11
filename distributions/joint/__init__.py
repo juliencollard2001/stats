10be41c5413d9b8c5524bb02134dfab7c00701e4
Julien Collard <juliencollard2001@gmail.com>
2024-04-11 15:04:19 -0400

    bug

--- distributions/joint/__init__.py
+++ distributions/joint/__init__.py
@@ -2,4 +2,4 @@ from .base import Joint
 from .independent import Independent
 from .gaussian import Gaussian
 from .copula_based import CopulaBased
-from .quantiles_copula import QuantilesGaussian
+from .quantiles_copula import QuantilesCopula
