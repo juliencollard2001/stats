b9bb783f6fbc1b78cb57d4b051184d578f31d707
Julien Collard <juliencollard2001@gmail.com>
2024-04-11 14:42:15 -0400

    BIC, AIC

--- distributions/util/__init__.py
+++ distributions/util/__init__.py
@@ -1,2 +1,2 @@
-from .metrics import log_likelihood
+from .metrics import log_likelihood, BIC, AIC
 from .plots import plot_marginals
--- distributions/util/metrics.py
+++ distributions/util/metrics.py
@@ -5,4 +5,12 @@ from jax import vmap
 from ..joint import Joint
 
 def log_likelihood(joint : Joint, data : Array) -> Array:
-    return jnp.sum(joint.logpdf(data)) / data.shape[0]
+    return jnp.sum(joint.logpdf(data))
+
+def BIC(joint : Joint, data : Array):
+    LL, k, N = log_likelihood(joint, data), joint.get_nb_params(), data.shape[0]
+    return - 2 * LL + jnp.log(N) * k
+
+def AIC(joint : Joint, data : Array):
+    LL, k, N = log_likelihood(joint, data), joint.get_nb_params(), data.shape[0]
+    return - 2/N * LL + 2 * k / N
